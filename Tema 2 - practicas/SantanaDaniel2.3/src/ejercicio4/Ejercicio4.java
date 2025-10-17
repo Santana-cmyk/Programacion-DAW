37bcda43402779f941de6d31363f880b0d8f172a
Santana-cmyk <indirana916@outlook.es>
2025-10-17 03:25:43 +0200

    For

--- Tema 2 - practicas/SantanaDaniel2.3/src/ejercicio4/Ejercicio4.java
+++ Tema 2 - practicas/SantanaDaniel2.3/src/ejercicio4/Ejercicio4.java
@@ -5,23 +5,30 @@ import java.util.Scanner;
 public class Ejercicio4 {
 
 	public static void main(String[] args) {
-		Scanner teclado = new Scanner(System.in);
-		
-		System.out.println("Dame un número: ");
-		 int num1 = teclado.nextInt();
-		System.out.println("Dame otro número: ");
-		 int num2 = teclado.nextInt();
-		System.out.println("Otro: ");
-		 int num3 = teclado.nextInt();
-		 
-		 
-		 int suma = num1+num2+num3;
-		 double media = suma/3;
-		
-		 while (num1 >0 || num2>0||num3>0) { 
-			 System.out.println("La suma es: " +suma);
-			 System.out.println("La media es: " +media);
-		 }
+		 Scanner teclado = new Scanner(System.in);
+
+	        int num, suma = 0, contador = 0;
+	        double media = 0;
+
+	        
+	        System.out.println("Dame un número (0 para terminar):");
+	        num = teclado.nextInt();
+	        
+	        suma = num;
+
+	        if (num >= 0) {
+	          while (num != 0) {
+	        	System.out.println("Otro: ");
+	  	        num = teclado.nextInt();
+		        contador=contador+1;
+		        suma = suma+num;
+		        media = (suma+num)/contador;
+	          } 
+		        System.out.println("La suma es: "+suma);
+		        System.out.println("La media es: "+media);
+	        } else {
+	            System.out.println("Error");
+	        }
 	}
 
 }
